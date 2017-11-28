010366e9f2be9ad16212146f3a795a236cc01505
HidetoshiYanagisawa <yanagisawa@ultrasevenstar.com>
2017-11-28 08:58:33 +0000

    都道府県市区町村データ格納

--- db/migrate/20171128082455_create_cities.rb
+++ db/migrate/20171128082455_create_cities.rb
@@ -2,7 +2,7 @@ class CreateCities < ActiveRecord::Migration[5.1]
   def change
     create_table :cities do |t|
       t.integer :pref_id
-      t.string :name, :limit => 6
+      t.string :name, :limit => 16
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+
+CSV.foreach("db/KEN_ALL.CSV", encoding: "Shift_JIS:UTF-8") do |row|
+  prefName = row[6]
+  cityName = row[7]
+  pref = Pref.find_or_create_by(:name => prefName)
+  City.find_or_create_by(:name => cityName, pref_id: pref.id)
+end
