5281567f280ff869b70a2d5807f0bfdc5ef37e3b
HidetoshiYanagisawa <yanagisawa@ultrasevenstar.com>
2017-11-29 07:23:21 +0000

    完成

--- db/migrate/20171128081749_create_libraries.rb
+++ db/migrate/20171128081749_create_libraries.rb
@@ -1,14 +1,14 @@
 class CreateLibraries < ActiveRecord::Migration[5.1]
   def change
     create_table :libraries do |t|
-      t.string :name, :limit => 32
+      t.string :name, :limit => 64
       t.string :lib_id, :limit => 6, :unique => true
       t.string :url, :limit => 256
       t.integer :pref_id
       t.integer :city_id
       t.string :address, :limit => 64
       t.string :post, :limit => 9
-      t.string :tel, :limit => 13
+      t.string :tel, :limit => 64
       t.decimal :lat, :precision => 9, :scale => 6
       t.decimal :lng, :precision => 9, :scale => 6
       t.integer :library_category_id
